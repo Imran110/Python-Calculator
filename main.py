e43571465ebbbbcde7715262e5cdedd637c6474c
Imran Haider <ImmI110@yahoo.com>
2020-04-24 01:44:16 +0500

    Python Calc

---
+++ main.py
@@ -0,0 +1,7 @@
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+add = int(num1) + int(num2)
+sub = int(num1) - int(num2)
+mul = int(num1) * int(num2)
+div = int(num1) / int(num2)
+print('\nAdd: {0}\n\nSubtract: {1}\n\nMultiply: {2}\n\nDivid: {3}'. format(add, sub, mul, div))
